51bb074e878a7c9b9b97e15fa90abb1732cdac00
Bruno Varella Peixoto <varellap@gmail.com>
2020-10-19 01:12:44 -0300

    removed useless comments

--- trabalho-pratico1/a_star.py
+++ trabalho-pratico1/a_star.py
@@ -1,9 +1,7 @@
 from common import *
 
 
-# This class represents a node
 class Node:
-    # Initialize the class
     def __init__(self, position: (), parent: ()):
         self.position = position
         self.parent = parent
@@ -11,15 +9,12 @@ class Node:
         self.h = 0  # Distance to goal node
         self.f = 0  # Total cost
 
-    # Compare nodes
     def __eq__(self, other):
         return self.position == other.position
 
-    # Sort nodes
     def __lt__(self, other):
         return self.f < other.f
 
-    # Print node
     def __repr__(self):
         return '({0},{1})'.format(self.position, self.f)
 
